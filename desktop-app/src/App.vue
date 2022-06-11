338792f19b13a48f30bf331285e96c317be6e84e
RublevD <danil.r@devhouse.pro>
2022-06-11 14:42:28 +0300

    WElcome screen

--- desktop-app/src/App.vue
+++ desktop-app/src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Welcome msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './use-cases/welcome/Welcome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Welcome
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style.css';
 </style>
