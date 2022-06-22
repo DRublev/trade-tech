a0c9fa258cc0aa1d5d74285629bf23139a5642ee
tikhanenkovd <tikhanenkovd@gmail.com>
2022-06-22 22:51:08 +0300

    add: google analytics package

--- desktop-app/src/main.ts
+++ desktop-app/src/main.ts
@@ -1,7 +1,13 @@
-import './plugins/fontawesome'
-import { createApp } from 'vue';
-import App from '@/ui/screens/App.vue';
-import '@/ui/assets/tailwind.css';
-import FontAwesomeIcon from './plugins/fontawesome';
+import "./plugins/fontawesome";
+import { createApp } from "vue";
+import App from "@/ui/screens/App.vue";
+import "@/ui/assets/tailwind.css";
+import FontAwesomeIcon from "./plugins/fontawesome";
+import VueGtag from "vue-gtag";
 
-createApp(App).component('fa', FontAwesomeIcon).mount('#app');
+createApp(App)
+  .component("fa", FontAwesomeIcon)
+  .use(VueGtag, {
+    config: { id: "G-3DFKMH5WG5" },
+  })
+  .mount("#app");
