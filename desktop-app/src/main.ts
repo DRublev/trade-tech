a00ce3a727ab19d63c42d2010ba0cc3643e75e9b
tikhanenkovd <tikhanenkovd@gmail.com>
2022-06-28 20:43:08 +0300

    add: vue-router 3

--- desktop-app/src/main.ts
+++ desktop-app/src/main.ts
@@ -4,10 +4,12 @@ import App from "@/ui/screens/App.vue";
 import "@/ui/assets/tailwind.css";
 import FontAwesomeIcon from "./plugins/fontawesome";
 import VueGtag from "vue-gtag";
+import router from "./plugins/router";
 
 createApp(App)
   .component("fa", FontAwesomeIcon)
   .use(VueGtag, {
     config: { id: "G-3DFKMH5WG5" },
   })
+  .use(router)
   .mount("#app");
