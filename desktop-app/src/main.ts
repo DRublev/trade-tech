86b1453a6867e13b226a0b6bd40f756ee7815331
tikhanenkovd <tikhanenkovd@gmail.com>
2022-07-05 08:32:10 +0300

    add: mixpanel

--- desktop-app/src/main.ts
+++ desktop-app/src/main.ts
@@ -5,11 +5,18 @@ import "@/ui/assets/tailwind.css";
 import FontAwesomeIcon from "./plugins/fontawesome";
 import VueGtag from "vue-gtag";
 import router from "./plugins/router";
+import VueMixpanel from "vue-mixpanel";
 
 createApp(App)
   .component("fa", FontAwesomeIcon)
   .use(VueGtag, {
     config: { id: "G-3DFKMH5WG5" },
+  })
+  .use(VueMixpanel, {
+    token: "435a28aafbd9be984c91c325107c6de0",
+    config: {
+      debug: true,
+    },
   })
   .use(router)
   .mount("#app");
